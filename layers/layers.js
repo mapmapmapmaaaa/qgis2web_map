216776fd1039ab9125ba448ad5c07acec1074b17
mapmapmapmaaaa <slabogchikova.s@gmail.com>
2025-02-04 17:38:54 +0700

    Update layers.js

--- layers/layers.js
+++ layers/layers.js
@@ -42,7 +42,7 @@ var lyr_building_2 = new ol.layer.Vector({
                 title: '<img src="styles/legend/building_2.png" /> building'
             });
 var format__3 = new ol.format.GeoJSON();
-var features__3 = format__3.readFeatures(json__3, 
+var features__3 = format__3.readFeatures(json_3, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
 var jsonSource__3 = new ol.source.Vector({
     attributions: ' ',
